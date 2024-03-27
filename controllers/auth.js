9ea65fb4bc338115237666c63adb808f86438b96
Mirko Rimac <mirkorimac98@gmail.com>
2024-03-27 07:09:35 +0100

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
+exports.register = (req, res) => {
+    console.log(req.body);
+
+    /* The same below. Its called destructuring in JS
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const passwordConfirm = req.body.passwordConfirm;
+    */
+
+    const { name, email, password, passwordConfirm } = req.body;
+
+    db.query()
+
+    res.send("Form submitted");
+}
